f78b5342b61f5dee3226eba4b541ade4dd3d02b7
quandh79 <dohongquantc@gmail.com>
2022-12-21 22:56:04 +0700

    1Add project files.

---
+++ T2203E_CSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using T2203E_CSharp.session1;
+using T2203E_CSharp.session2;
+using T2203E_CSharp.bt;
+
+public class Program
+{
+    static void Main()
+    {
+        /*   Human h = new Human();
+           h.run();
+           h.Age = 19;
+           h.Email = "abc@gmail.com";// properties
+           Console.WriteLine(h.Email);
+           Console.WriteLine(h.Age);
+
+           Student s = new Student();
+           List<string> list = new List<string>();
+           list.Add("hello");
+           list.Add("world");
+           list.Add("morning");
+           for(int i = 0; i< list.Count; i++)
+           {
+               Console.WriteLine(list[i]);
+           }
+        */
+
+        Phanso p1 = new Phanso();
+        p1.tuso = 1;
+        p1.mauso = 3;
+        Phanso p2 = new Phanso();
+        p2.tuso = 3;
+        p2.mauso = 5;
+
+        Phanso f3 = Phanso.Add(p1, p2);
+        f3.Print();
+    }
+}
---
+++ T2203E_CSharp/session2/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T2203E_CSharp.session1;
+
+namespace T2203E_CSharp.session2
+{
+    internal class Student: Human
+
+    {
+        public string[] telephone = new string[3];
+      
+        public Student(string name, int age)
+        {
+            this.name = name;
+        }
+
+        public Student()
+        {
+        }
+
+        public void Study()
+        {
+            base.run();
+            this.run();
+        }
+        public new void Run()
+        {
+
+        }
+
+        public override void Eat()
+        {
+            base.Eat();
+        }
+    }
+}
